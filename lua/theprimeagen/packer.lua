a8bb6d8611c4c1d837cbf8bae0bb00c7a4da18ce
reikrom <reikrom.lv@gmail.com>
2024-02-05 22:59:38 +0200

    starting to make it my own

--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -8,16 +8,16 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
+	  'nvim-telescope/telescope.nvim', tag = '0.1.5',
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
 
   use({
-	  'rose-pine/neovim',
-	  as = 'rose-pine',
+	  'oxfist/night-owl.nvim',
+	  as = 'night-owl',
 	  config = function()
-		  vim.cmd('colorscheme rose-pine')
+		  vim.cmd('colorscheme night-owl')
 	  end
   })
 
